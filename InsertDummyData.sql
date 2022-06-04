16df707cc8e2869ea405bb1fc4d8dfd3c97326e1
Ranomenjanahary <hei.jaonary@gmail.com>
2022-06-04 19:03:44 +0300

    feat: insert Utiliser data

--- InsertDummyData.sql
+++ InsertDummyData.sql
@@ -45,3 +45,13 @@ insert into vehicules (nombre_places, places_dispo, matricule) values (24, 7, '4
 insert into vehicules (nombre_places, places_dispo, matricule) values (24, 8, '4725919');
 insert into vehicules (nombre_places, places_dispo, matricule) values (24, 9, '4808146');
 insert into vehicules (nombre_places, places_dispo, matricule) values (24, 10, '4275558');
+
+-- insert utiliser
+insert into utiliser (id_vehicule, id_voyage) values (1, 2);
+insert into utiliser (id_vehicule, id_voyage) values (2, 3);
+insert into utiliser (id_vehicule, id_voyage) values (3, 5);
+insert into utiliser (id_vehicule, id_voyage) values (4, 7);
+insert into utiliser (id_vehicule, id_voyage) values (5, 4);
+insert into utiliser (id_vehicule, id_voyage) values (6, 10);
+insert into utiliser (id_vehicule, id_voyage) values (9, 6);
+insert into utiliser (id_vehicule, id_voyage) values (10, 1);
