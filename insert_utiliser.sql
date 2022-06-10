7e4d42dace779f536f1f7a6b342c715ecef4419e
P3RM1_D3N1ED <72337259+Tsirimaholy@users.noreply.github.com>
2022-06-10 17:41:09 +0300

    Delete insert_utiliser.sql

--- insert_utiliser.sql
+++
@@ -1,9 +0,0 @@
--- insert utiliser
-insert into utiliser (id_vehicule, id_voyage) values (1, 2);
-insert into utiliser (id_vehicule, id_voyage) values (2, 3);
-insert into utiliser (id_vehicule, id_voyage) values (3, 5);
-insert into utiliser (id_vehicule, id_voyage) values (4, 7);
-insert into utiliser (id_vehicule, id_voyage) values (5, 4);
-insert into utiliser (id_vehicule, id_voyage) values (6, 10);
-insert into utiliser (id_vehicule, id_voyage) values (9, 6);
-insert into utiliser (id_vehicule, id_voyage) values (10, 1);
