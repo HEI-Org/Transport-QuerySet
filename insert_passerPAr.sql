d944fd12c56d9f737003472c1d4c0d20d74e101e
P3RM1_D3N1ED <72337259+Tsirimaholy@users.noreply.github.com>
2022-06-10 17:40:06 +0300

    Delete insert_passerPAr.sql

--- insert_passerPAr.sql
+++
@@ -1,8 +0,0 @@
--- insert passer par
-insert into passer_par (id_ville, id_voyage) values (2,130);
-insert into passer_par (id_ville, id_voyage) values (10,130);
-insert into passer_par (id_ville, id_voyage) values (6,130);
-insert into passer_par (id_ville, id_voyage) values (1,130);
-insert into passer_par (id_ville, id_voyage) values (4,17);
-insert into passer_par (id_ville, id_voyage) values (5,17);
-insert into passer_par (id_ville, id_voyage) values (3,17);
