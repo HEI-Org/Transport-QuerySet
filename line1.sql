aab9508bfc8b93dde5f348f3aae5cfb35392283e
Ranomenjanahary <hei.jaonary@gmail.com>
2022-06-10 13:17:07 +0300

    feat: requete select line1 et 2

---
+++ line1.sql
@@ -0,0 +1,3 @@
+-- Afficher les depart de nuit --
+
+SELECT * FROM voyage WHERE heure_depart <= '06:00 AM' AND heure_depart >= '05:59 PM' ;
---
+++ line2.sql
@@ -0,0 +1,3 @@
+-- Afficher les depart du jour --
+
+SELECT * FROM voyage WHERE heure_depart >= '06:00 AM' AND heure_depart <= '05:59 PM' ;
